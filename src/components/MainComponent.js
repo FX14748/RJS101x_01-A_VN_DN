c3189d1508436f91a5ac3bf1f9dbd7744738a1a2
Dino Nguyen <dinhntqFX14748@funix.edu.vn>
2022-01-20 21:45:06 +0700

    first commit

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,29 +1,37 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Menu from './MenuComponent';
 //import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import Footer from './FooterComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
 
   constructor(props) {
     super(props);
+
     this.state = {
         dishes: DISHES,
-        selectedDish: null
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-  }
-
   render() {
+
+    const Homepage = () => {
+      return(
+        <Home />
+      )
+    }
     return (
       <div>
         <Header />
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+        <Switch>
+          <Route path="/home" component={Homepage} />
+          <Route exact path="/menu" cpmponent={() => <Menu dishes={this.state.dishes} />} />
+          <Redirect to="/home" />
+        </Switch>
         <Footer />
       </div>
     );
