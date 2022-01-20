1b719e68ddd52f1cf808bd30e5ceb736f6bf7440
Dino Nguyen <dinhntqFX14748@funix.edu.vn>
2022-01-20 13:09:03 +0700

    first commit

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import Header from './HeaderComponent';
 import Menu from './MenuComponent';
 //import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
+import Footer from './FooterComponent';
 
 class Main extends Component {
 
@@ -21,12 +22,9 @@ class Main extends Component {
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion hah hah </NavbarBrand>
-          </div>
-        </Navbar>
+        <Header />
         <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+        <Footer />
       </div>
     );
   }
