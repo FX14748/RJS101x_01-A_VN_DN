20e35db9a44bc26c7c46b44c3112a35c5f137e4e
Dino Nguyen <dinhntqFX14748@funix.edu.vn>
2022-01-25 21:52:05 +0700

    Redux Thunk

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,6 +1,8 @@
 import { Card, CardImg, CardText, CardBody,
     CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
+
 
     function RenderDish({dish, favorite, postFavorite}) {
             return(
@@ -45,7 +47,25 @@ import { Link } from 'react-router-dom';
 
     
     const DishDetail = (props) => {
-        if (props.dish != null)        
+        if (props.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else if (props.dish != null)   
             return (
                 <div className="container">
                     <div className="row">
